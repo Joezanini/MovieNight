eb55ac39d1e6d55c4c67e67bbc9c8de33b2fd136
joezanini <jzanini@ucsc.edu>
2021-03-23 17:38:05 -0700

    import stuff

---
+++ venv/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for, jsonify, request, make_response
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import session
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from flask_login import current_user
+import logging
+import random
+app = Flask(__name__)
+
